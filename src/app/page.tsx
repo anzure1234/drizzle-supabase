7a9a2f0f6b50e69adefdda45a701fb178b0d78bc
Dung1109 <124704925+Dung1109@users.noreply.github.com>
2024-09-07 13:56:00 +0700

    add dummy file to make git happy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
 export default function Home() {
-    return <>Home</>;
+    return (
+        <div className="flex min-h-screen flex-col items-center justify-center py-2">
+            <main className="flex w-full flex-col items-center justify-center px-6 py-10 text-center">
+                <h1 className="mb-6 text-4xl font-bold tracking-tight text-gray-900">
+                    Drizzle Supabase
+                </h1>
+                <p className="mb-6 text-lg font-medium text-gray-600">
+                    A simple starter for Drizzle and Supabase
+                </p>
+            </main>
+        </div>
+    )
 }
